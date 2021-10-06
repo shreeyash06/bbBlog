dd4289c77b06be9dba0433d599642190dda06221
shreeyash06 <91952544+shreeyash06@users.noreply.github.com>
2021-10-06 12:37:25 +0530

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+// JavaScript code
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('shreeyash');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
